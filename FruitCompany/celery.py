5950a94585fe3a2c815664307bd0579f720868d2
Rur1k <falgerok.master@gmail.com>
2022-02-10 10:58:40 +0200

    сократил количество, тасков 0шгддхлокз

--- FruitCompany/celery.py
+++ FruitCompany/celery.py
@@ -14,48 +14,46 @@ app.autodiscover_tasks()
 
 app.conf.beat_schedule = {
     'add-apple-every-6-second': {
-        'task': 'admin_panel.tasks.replenishment_warehouse_apple',
+        'task': 'admin_panel.tasks.replenishment_warehouse_fruit',
         'schedule': timedelta(seconds=6),
+        'args': (1, 1, 10, 1),
     },
     'add-banana-every-9-second': {
-        'task': 'admin_panel.tasks.replenishment_warehouse_banana',
+        'task': 'admin_panel.tasks.replenishment_warehouse_fruit',
         'schedule': timedelta(seconds=9),
+        'args': (2, 10, 20, 1),
     },
     'add-pineapple-every-12-second': {
-        'task': 'admin_panel.tasks.replenishment_warehouse_pineapple',
+        'task': 'admin_panel.tasks.replenishment_warehouse_fruit',
         'schedule': timedelta(seconds=12),
+        'args': (3, 1, 10, 1),
     },
     'add-peach-every-15-second': {
-        'task': 'admin_panel.tasks.replenishment_warehouse_peach',
+        'task': 'admin_panel.tasks.replenishment_warehouse_fruit',
         'schedule': timedelta(seconds=15),
+        'args': (4, 5, 15, 1),
     },
     'sell-apple-every-15-second': {
-        'task': 'admin_panel.tasks.sell_apple',
+        'task': 'admin_panel.tasks.sell_fruit',
         'schedule': timedelta(seconds=15),
+        'args': (1, 1, 10, 1),
     },
     'sell-banana-every-12-second': {
-        'task': 'admin_panel.tasks.sell_banana',
+        'task': 'admin_panel.tasks.sell_fruit',
         'schedule': timedelta(seconds=12),
+        'args': (2, 1, 30, 1),
     },
     'sell-pineapple-every-9-second': {
-        'task': 'admin_panel.tasks.sell_pineapple',
+        'task': 'admin_panel.tasks.sell_fruit',
         'schedule': timedelta(seconds=9),
+        'args': (3, 1, 10, 1),
     },
     'sell-peach-every-9-second': {
-        'task': 'admin_panel.tasks.sell_peach',
+        'task': 'admin_panel.tasks.sell_fruit',
         'schedule': timedelta(seconds=6),
+        'args': (4, 1, 20, 1),
     },
 }
 
-# app.conf.beat_schedule = {
-#     'add-apple-every-6-second': {
-#         'task': 'admin_panel.tasks.replenishment_warehouse_apple_wallet',
-#         'schedule': timedelta(seconds=6),
-#     },
-#     'sell-apple-every-15-second': {
-#         'task': 'admin_panel.tasks.sell_apple_wallet',
-#         'schedule': timedelta(seconds=15),
-#     },
-# }
 
 
--- admin_panel/tasks.py
+++ admin_panel/tasks.py
@@ -10,117 +10,34 @@ def replenishment_warehouse():
 
 
 # Пополнение склада
-# @app.task
-# def replenishment_warehouse_apple_wallet():
-#     obj = Fruit.objects.get(pk=1)
-#     wallet = Wallet.objects.get(pk=1)
-#
-#     add = random.randrange(10)
-#     if (add * obj.price_buy) > wallet.money:
-#         return f'ERRORS: Не достаточно денег для закупки яблок!'
-#     else:
-#         count = obj.count + add
-#         money = wallet.money - (add * obj.price_buy)
-#         Fruit.objects.filter(pk=1).update(count=count)
-#         Wallet.objects.filter(pk=1).update(money=money)
-#         return f'SUCCESS: Склад пополнен на {add} яблок! Заплачено:{add * obj.price_buy}'
-
-
 @app.task
-def replenishment_warehouse_apple():
-    obj = Fruit.objects.get(pk=1)
-    add = random.randrange(10)
-    count = obj.count + add
-    Fruit.objects.filter(pk=1).update(count=count)
-    return f'SUCCESS: Склад пополнен на {add} яблок!'
+def replenishment_warehouse_fruit(fruit_id, start=None, stop=None, step=None):
+    obj = Fruit.objects.get(pk=fruit_id)
+    wallet = Wallet.objects.get(pk=1)
 
+    add = random.randrange(start, stop, step)
+    if (add * obj.price_buy) > wallet.money:
+        return f'ERRORS: Не достаточно денег для закупки {add} {obj.name}!'
+    else:
+        count = obj.count + add
+        money = wallet.money - (add * obj.price_buy)
+        Fruit.objects.filter(pk=fruit_id).update(count=count)
+        Wallet.objects.filter(pk=1).update(money=money)
+        return f'SUCCESS: Склад пополнен на {add} {obj.name}! Заплачено:{add * obj.price_buy}'
+
+
+# Продажа фруктов
 @app.task
-def replenishment_warehouse_banana():
-    obj = Fruit.objects.get(pk=2)
-    add = random.randrange(10, 20, 1)
-    count = obj.count + add
-    Fruit.objects.filter(pk=2).update(count=count)
-    return f'SUCCESS: Склад пополнен на {add} бананов!'
+def sell_fruit(fruit_id, start=None, stop=None, step=None):
+    obj = Fruit.objects.get(pk=fruit_id)
+    wallet = Wallet.objects.get(pk=1)
 
-
-@app.task
-def replenishment_warehouse_pineapple():
-    obj = Fruit.objects.get(pk=3)
-    add = random.randrange(10)
-    count = obj.count + add
-    Fruit.objects.filter(pk=3).update(count=count)
-    return f'SUCCESS: Склад пополнен на {add} ананасов!'
-
-
-@app.task
-def replenishment_warehouse_peach():
-    obj = Fruit.objects.get(pk=4)
-    add = random.randrange(5, 15, 1)
-    count = obj.count + add
-    Fruit.objects.filter(pk=4).update(count=count)
-    return f'SUCCESS: Склад пополнен на {add} персиков!'
-
-
-# Расхищение склада
-# @app.task
-# def sell_apple_wallet():
-#     obj = Fruit.objects.get(pk=1)
-#     wallet = Wallet.objects.get(pk=1)
-#
-#     count_sell = random.randrange(10)
-#     if count_sell > obj.count:
-#         return f'ERROR: Невозможно продать {count_sell} яблок! Недостаток на складе.'
-#     else:
-#         count = obj.count - count_sell
-#         money = wallet.money + count_sell*obj.price_sell
-#         Fruit.objects.filter(pk=1).update(count=count)
-#         Wallet.objects.filter(pk=1).update(money=money)
-#         return f'SUCCESS: С склада продано {count_sell} яблок! Заработано: {count_sell*obj.price_sell}'
-
-@app.task
-def sell_apple():
-    obj = Fruit.objects.get(pk=1)
-    count_sell = random.randrange(10)
+    count_sell = random.randrange(start, stop, step)
     if count_sell > obj.count:
-        return f'ERROR: Невозможно продать {count_sell} яблок! Недостаток на складе.'
+        return f'ERROR: Невозможно продать {count_sell} {obj.name}! Недостаток на складе.'
     else:
         count = obj.count - count_sell
-        Fruit.objects.filter(pk=1).update(count=count)
-        return f'SUCCESS: С склада продано {count_sell} яблок! Заработано: {count_sell*obj.price_sell}'
-
-
-@app.task
-def sell_banana():
-    obj = Fruit.objects.get(pk=2)
-    count_sell = random.randrange(30)
-    if count_sell > obj.count:
-        return f'ERROR: Невозможно продать {count_sell} бананов! Недостаток на складе.'
-    else:
-        count = obj.count - count_sell
-        Fruit.objects.filter(pk=2).update(count=count)
-        return f'SUCCESS: С склада продано {count_sell} бананов! Заработано: {count_sell*obj.price_sell}'
-
-
-@app.task
-def sell_pineapple():
-    obj = Fruit.objects.get(pk=3)
-    count_sell = random.randrange(10)
-    if count_sell > obj.count:
-        return f'ERROR: Невозможно продать {count_sell} ананасов! Недостаток на складе.'
-    else:
-        count = obj.count - count_sell
-        Fruit.objects.filter(pk=3).update(count=count)
-        return f'SUCCESS: С склада продано {count_sell} ананасов! Заработано: {count_sell*obj.price_sell}'
-
-
-@app.task
-def sell_peach():
-    obj = Fruit.objects.get(pk=4)
-    count_sell = random.randrange(20)
-    if count_sell > obj.count:
-        return f'ERROR: Невозможно продать {count_sell} персиков! Недостаток на складе.'
-    else:
-        count = obj.count - count_sell
-        Fruit.objects.filter(pk=5).update(count=count)
-        return f'SUCCESS: С склада продано {count_sell} персиков! Заработано: {count_sell*obj.price_sell}'
-
+        money = wallet.money + count_sell*obj.price_sell
+        Fruit.objects.filter(pk=fruit_id).update(count=count)
+        Wallet.objects.filter(pk=1).update(money=money)
+        return f'SUCCESS: С склада продано {count_sell} {obj.name}! Заработано: {count_sell * obj.price_sell}'
