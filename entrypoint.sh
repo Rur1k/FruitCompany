351549e51facceca7843d159a7f5bfec26c76c52
Rur1k <falgerok.master@gmail.com>
2022-02-21 11:58:10 +0200

    закончил проект, подготовил его к деплою

--- entrypoint.sh
+++
@@ -1,9 +0,0 @@
-#! /bin/bash
-
-python manage.py makemigrations --no-input
-
-python manage.py migrate --no-input
-
-python manage.py collectstatic --no-input
-
-exec uvicorn FruitCompany.asgi:application --host 0.0.0.0 --port 8000
